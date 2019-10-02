b40d690115059d8e8fc7d7659c365372a61a85e8
李進興 Vulcan <vulcan@miniasp.com>
2019-10-02 12:54:47 +0800

    手勢操作

---
+++ ch7/GestureGame/GestureGame/GestureGame/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using Xamarin.Forms;
+
+namespace GestureGame.ViewModels
+{
+    public class MainPageViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public string Message { get; set; }
+
+        public Command<string> TapCommand { get; set; }
+        public MainPageViewModel()
+        {
+            TapCommand = new Command<string>(x =>
+            {
+                Message = x;
+            });
+        }
+    }
+}
