55d4a3b2750b3a9a265186e5e6b1bae8ab53c950
李進興 Vulcan <vulcan@miniasp.com>
2019-10-04 20:10:06 +0800

    使用 XAML 標記宣告語言來開發 Xamarin.Forms App

---
+++ ch3/FirstXAMLStyle/FirstXAML/FirstXAML/QuestionPage.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FirstXAML
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class QuestionPage : ContentPage
+    {
+        int value1 = 0;
+        int value2 = 0;
+
+        public QuestionPage()
+        {
+            InitializeComponent();
+            CreateQuestion();
+            btnReQuestion.Clicked += (s, e) =>
+            {
+                CreateQuestion();
+            };
+            btnSubmit.Clicked += (s, e) =>
+            {
+                int sum = int.Parse(entAnswer.Text);
+                if ((value1 + value2) == sum)
+                {
+                    lbMessage.Text = "答對了";
+                }
+                else
+                {
+                    lbMessage.Text = "答錯了";
+                }
+            };
+        }
+
+        void CreateQuestion()
+        {
+            Random random = new Random();
+            value1 = random.Next(10, 99);
+            value2 = random.Next(10, 99);
+
+            lbValue1.Text = $"{value1}";
+            lbValue2.Text = $"{value2}";
+
+            lbQuestion.Text = $"請問 {value1} + {value2} = 多少?";
+            lbMessage.Text = "";
+        }
+    }
+}
