2b042b2ea73e3359b9b7073d92352c52a3b1e497
李進興 Vulcan <vulcan@miniasp.com>
2019-10-05 12:46:16 +0800

    資料綁定的練習專案 XAML - XAML

---
+++ ch4/XAMLBinding/XAMLBinding/XAMLBinding/CLRClass.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XAMLBinding
+{
+    public class CLRClass
+    {
+        public static string CLRProperty { get; set; } = "來自 CLR 的物件值";
+    }
+}
