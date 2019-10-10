0ab53cf64ff0b707e9634b2b224da8d2e600d52c
李進興 Vulcan <vulcan@miniasp.com>
2019-10-10 12:30:28 +0800

    EventToCommad Behavior

---
+++ ch6/Event2Command/Event2Command/Event2Command/MyBindingContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Event2Command
+{
+    public class MyBindingContext : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        double maxLength = 300;
+        public double SliderValue { get; set; }
+        public double CurrentValue { get; set; }
+        public Command<ValueChangedEventArgs> SliderChangeCommand { get; set; }
+        public MyBindingContext()
+        {
+            SliderChangeCommand = new Command<ValueChangedEventArgs>(x =>
+            {
+                CurrentValue = maxLength * x.NewValue;
+            });
+        }
+    }
+}
