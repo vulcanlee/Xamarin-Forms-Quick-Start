6b435f0e41b732627ec268112f3a08a30c33ff43
李進興 Vulcan <vulcan@miniasp.com>
2019-10-12 12:56:48 +0800

    手勢操作

--- ch8/GestureGame/GestureGame/GestureGame/MyBindingContext.cs
+++ ch8/GestureGame/GestureGame/GestureGame/MyBindingContext.cs
@@ -4,15 +4,15 @@ using System.ComponentModel;
 using System.Text;
 using Xamarin.Forms;
 
-namespace GestureGame.ViewModels
+namespace GestureGame
 {
-    public class MainPageViewModel : INotifyPropertyChanged
+    public class MyBindingContext : INotifyPropertyChanged
     {
         public event PropertyChangedEventHandler PropertyChanged;
         public string Message { get; set; }
 
         public Command<string> TapCommand { get; set; }
-        public MainPageViewModel()
+        public MyBindingContext()
         {
             TapCommand = new Command<string>(x =>
             {
